b522981c978b09c9e56b52a6a96b88f64f39d7cd
Ruan-Miguel <miguelcoelho148@gmail.com>
2020-06-22 16:07:37 -0300

    criacao do componente Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react'
+import { ThemeProvider } from '@material-ui/core'
+import { makeStyles, unstable_createMuiStrictModeTheme as unstableCreateMuiStrictModeTheme } from '@material-ui/core/styles'
+import Drawer from '@material-ui/core/Drawer'
+import List from '@material-ui/core/List'
+import Divider from '@material-ui/core/Divider'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import InboxIcon from '@material-ui/icons/MoveToInbox'
+import MailIcon from '@material-ui/icons/Mail'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+
+const theme = unstableCreateMuiStrictModeTheme()
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  },
+  list: {
+    width: 250
+  }
+})
+
+export default function Header () {
+  const classes = useStyles()
+  const [state, setState] = useState(false)
+
+  const toggleDrawer = (open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return
+    }
+
+    setState(open)
+  }
+
+  const list = () => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  )
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton onClick={toggleDrawer(true)} edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" className={classes.title}>
+            Maps
+        </Typography>
+      </Toolbar>
+      <ThemeProvider theme={theme}>
+        <Drawer anchor={'left'} open={state} onClose={toggleDrawer(false)}>
+          {list()}
+        </Drawer>
+      </ThemeProvider>
+    </AppBar>
+  )
+}
