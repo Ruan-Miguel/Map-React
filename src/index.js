c92718d788a6acaf70f88338789f910dd69b1103
Ruan-Miguel <miguelcoelho148@gmail.com>
2020-06-22 16:10:10 -0300

    padronizaccao das cores

--- src/index.js
+++ src/index.js
@@ -3,9 +3,24 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core'
+import indigo from '@material-ui/core/colors/indigo'
+import teal from '@material-ui/core/colors/teal'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: teal[600]
+    },
+    secondary: indigo
+  }
+})
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
